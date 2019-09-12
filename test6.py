1ed7ffa5386f5df6e4409bba069f4c25338e126d
magcofen <farzin.mag@gmail.com>
2019-09-12 16:43:52 +0430

    commented

--- test6.py
+++ test6.py
@@ -4,7 +4,7 @@ import utime ##baray ejad delay va jologire az hang karcan
 import gc ##moderiat hafeze
 import urequests as requests ##baraye ertebat ba internet
 from machine import Pin,ADC ##tanzem halet pin ha
-adc=machine.ADC(0) ##tabdel dade aeryale be digital
+adc=machine.ADC(0) ##taref pin analog
 gc.enable()
 s=network.WLAN(network.STA_IF) ##sazekar client kardan mothole
 s.active(True)
@@ -17,17 +17,18 @@ dict={
 } ##dictionary baraye daryaft response
 dade=ujson.dumps(dade) ##seralize kardan dade be form json
 while True:
-  ##response=requests.post(url,data=dade,headers={'Connection':'close'})
-  response=requests.get(url , data=dade)
-  print(response.text)
-  ##x=response.text
-  ##dict=ujson.load(x)
-  ##responses=ujson.load(response)
-  ##response=requests.get(url)
-  ##responses=response.json()
-  ##print(response.text)
-  ##state=responses["abas"]
-  ##print(state)
-  utime.sleep(3)
-  response.close()
-  gc.collect()
+    data=adc.read()##tabdel etelat az analog be digital
+    ##response=requests.post(url,data=dade,headers={'Connection':'close'})
+    response=requests.get(url , data=dade)
+    print(response.text)
+    ##x=response.text
+    ##dict=ujson.load(x)
+    ##responses=ujson.load(response)
+    ##response=requests.get(url)
+    ##responses=response.json()
+    ##print(response.text)
+    ##state=responses["abas"]
+    ##print(state)
+    utime.sleep(3)
+    response.close()
+    gc.collect()

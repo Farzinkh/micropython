2f25316e80d1f6b2c1d8304c46c9980f740a946f
magcofen <farzin.mag@gmail.com>
2019-09-12 16:29:33 +0430

    commented

--- test6.py
+++ test6.py
@@ -1,21 +1,21 @@
-import network
-import ujson
-import utime
-import gc
-import urequests as requests
-from machine import Pin,ADC
-adc=machine.ADC(0)
+import network ##baraye vasl kardan nodemcu be wifi va dastresi be internet
+import ujson ##formot ersal va daryaft request va response az server
+import utime ##baray ejad delay va jologire az hang karcan
+import gc ##moderiat hafeze
+import urequests as requests ##baraye ertebat ba internet
+from machine import Pin,ADC ##tanzem halet pin ha
+adc=machine.ADC(0) ##tabdel dade aeryale be digital
 gc.enable()
-s=network.WLAN(network.STA_IF)
+s=network.WLAN(network.STA_IF) ##sazekar client kardan mothole
 s.active(True)
-s.connect('Anonymous','6339tktz6z')
+s.connect('Anonymous','6339tktz6z') ##ssid va password wifi
 url='http://192.168.43.79:8000/delta/tele/led'
 dade={
 'state':'gogol'
-}
+} ##dade ke ghast ersal on be server ra darem
 dict={
-}
-dade=ujson.dumps(dade)
+} ##dictionary baraye daryaft response
+dade=ujson.dumps(dade) ##seralize kardan dade be form json
 while True:
   ##response=requests.post(url,data=dade,headers={'Connection':'close'})
   response=requests.get(url , data=dade)
@@ -27,7 +27,7 @@ while True:
   ##responses=response.json()
   ##print(response.text)
   ##state=responses["abas"]
- ##print(state)
+  ##print(state)
   utime.sleep(3)
   response.close()
   gc.collect()

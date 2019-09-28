eaf05f36e10831c436368ffe15b88892b11cbeae
magcofen <farzin.mag@gmail.com>
2019-09-28 13:58:12 +0330

    0.3

--- test6.py
+++ test6.py
@@ -6,6 +6,9 @@ import urequests as requests ##baraye ertebat ba internet
 from machine import Pin,ADC ##tanzem halet pin ha
 adc=machine.ADC(0) ##taref pin analog
 gc.enable()
+led=Pin(2,Pin.OUT) 
+##pinMode(0, INPUT_PULLUP)
+button=Pin(0,Pin.IN)
 def connect():
   ssid = 'Anonymous'
   password =  '6339tktz6z'    ##ssid va password wifi
@@ -23,32 +26,43 @@ def connect():
     utime.sleep(3)
     pass
   print("Connection successful")  
-  
-connect()    
+     
 url='http://192.168.43.79:8000/delta/tele/led/'
-dade={
-'moisture':677
-} ##dade ke ghast ersal on be server ra darem
 dict={
 } ##dictionary baraye daryaft response
-dade_dic=ujson.dumps(dade)
+
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
 while True:
-    data=adc.read()##tabdel etelat az analog be digital
-    ##response=requests.post(url,data=dade,headers={'Connection':'close'})
-    send=requests.request('POST', url, data=dade_dic,headers=headers, stream=False)
-    print(send.status_code)
-    ##response=requests.get(url)
-    ##x=send.json()
-    print(send.text)
-    ##dict=ujson.load(x)
-    ##responses=ujson.load(response)
-    ##response=requests.get(url)
-    ##responses=response.json()
-    ##state=responses["abas"]
-    ##print(state)
-    utime.sleep(1)
-    send.close()
-    gc.collect()
+  if button.value() == 0:
+    while True:
+      connect() 
+      data=adc.read()##tabdel etelat az analog be digital
+      dade={'moisture':data}##dade ke ghast ersal on be server ra darem
+      dade_dic=ujson.dumps(dade)
+      ##response=requests.post(url,data=dade,headers={'Connection':'close'})
+      send=requests.request('POST', url, data=dade_dic,headers=headers, stream=False)
+      ##print(send.status_code)
+      ##response=requests.get(url)
+      x=send.json()
+      x=x['state']
+      if x=='on':
+        led.value(0)
+      else  :
+        led.value(1)
+      ##dict=ujson.load(x)
+      ##responses=ujson.load(response)
+      ##response=requests.get(url)
+      ##responses=response.json()
+      ##state=responses["abas"]
+      ##print(state)
+      utime.sleep(1)
+      send.close()
+      gc.collect()
+  else: 
+    utime.sleep(0.5)        ## press flash button to run
+    pass
+
+
 
 
